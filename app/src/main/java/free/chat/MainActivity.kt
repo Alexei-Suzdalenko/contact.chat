c38142fb9bb97633b0697a94ae4d598b41446f4f
Alexei-Suzdalenko <alexei.saron@gmail.com>
2021-10-10 23:54:18 +0200

    second

--- app/src/main/java/free/chat/MainActivity.kt
+++ app/src/main/java/free/chat/MainActivity.kt
@@ -1,16 +1,16 @@
 package free.chat
-
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import free.chat.ui.main.SectionsPagerAdapter
 import free.chat.databinding.ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -32,5 +32,23 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+
+    //   // Write a message to the database
+    //   val database = Firebase.database
+    //   val myRef = database.getReference("message")
+    //   myRef.setValue("Hello, World!")
+
+
+    //   val db = Firebase.firestore
+    //   val user = hashMapOf("first" to "Ada", "last" to "Lovelace", "born" to 1815)
+
+    //   db.collection("users").add(user).addOnSuccessListener { documentReference ->
+    //           Log.d("tag", "DocumentSnapshot added with ID: ${documentReference.id}")
+    //       }
+    //       .addOnFailureListener { e ->
+    //           Log.w("tag", "Error adding document", e)
+    //       }
+
     }
 }
